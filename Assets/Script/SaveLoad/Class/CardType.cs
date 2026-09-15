ef5982ede82d3cc1c735854b10cc85c75d50b083
Zindda_ <89469092+zindalredda@users.noreply.github.com>
2026-09-15 09:58:27 +0900

    작업 시작

---
+++ Assets/Script/SaveLoad/Class/CardType.cs
@@ -0,0 +1,19 @@
+namespace Alice
+{
+    public enum CardType
+    {
+        None,
+        Spades0,
+        Spades1,
+        Spades2,
+        Diamonds0,
+        Diamonds1,
+        Diamonds2,
+        Hearts0,
+        Hearts1,
+        Hearts2,
+        Clovers0,
+        Clovers1,
+        Clovers2,
+    }
+}
---
+++ Assets/Script/SaveLoad/Class/SaveData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Alice
+{
+    public class SaveData
+    {
+
+    }
+
+    public class PlayerData
+    {
+        public int playerHp;
+        public int playerMaxHp;
+        public CardType firstCard;
+        public CardType secondCard;
+    }
+}
